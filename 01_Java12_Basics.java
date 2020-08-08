fe1239fc8f25edeacd41834897df40a6e55284c3
Ninad Gawad <ninadgawad@gmail.com>
2020-08-07 23:49:20 -0400

    Create 01_Java12_Basics.java

---
+++ 01_Java12_Basics.java
@@ -0,0 +1,11 @@
+// Java 12 
+package ninad;
+
+public class HelloWorld {
+
+	public static void main(String args[]){
+		System.out.println("Hello World");
+    Version version = java.lang.Runtime.version();
+    System.out.println("Java Version = "+version);
+	}
+}
