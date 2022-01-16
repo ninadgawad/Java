860bebdd472fb4f0b6027a63804114af1e98b9c4
Ninad Gawad <ninadgawad@gmail.com>
2022-01-15 22:34:31 -0500

    Update 01_Java12_Basics.java

--- 01_Java12_Basics.java
+++ 01_Java12_Basics.java
@@ -1,11 +1,13 @@
 // Java 12 
 package ninad;
 
+// Class Name
 public class HelloWorld {
 
+	// Main menthod
 	public static void main(String args[]){
 		System.out.println("Hello World");
-    Version version = java.lang.Runtime.version();
-    System.out.println("Java Version = "+version);
+    		Version version = java.lang.Runtime.version();
+    		System.out.println("Java Version = "+version);
 	}
 }
